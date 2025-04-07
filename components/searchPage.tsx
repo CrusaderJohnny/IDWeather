cd2663675f97887c62606bc9eb81ea1dce415be7
Derk-L <derekleongz9@gmail.com>
2025-04-07 17:46:04 -0600

    no means no

--- components/searchPage.tsx
+++ components/searchPage.tsx
@@ -46,7 +46,6 @@ export default function SearchPage() {
             [
                 {
                     text: 'No',
-                    onPress: () => goBackWithLocation(location),
                     style: 'cancel',
                 },
                 {
