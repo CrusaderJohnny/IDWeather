694808c77adfe4d68ffd6b4b43f2fbaf28cde753
Derk-L <derekleongz9@gmail.com>
2025-04-07 18:13:05 -0600

    persist last searched

--- app/index.tsx
+++ app/index.tsx
@@ -3,6 +3,7 @@ import { useLocalSearchParams, useRouter } from "expo-router";
 import WeatherApp from "../components/weatherApp";
 import Ionicons from '@expo/vector-icons/Ionicons';
 import { useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function App() {
     const router = useRouter();
@@ -11,10 +12,27 @@ export default function App() {
     const [longs, setLongs] = useState(-114.08529);
 
     useEffect(() => {
-        if (params.lat && params.lon) {
-            setLats(parseFloat(params.lat as string));
-            setLongs(parseFloat(params.lon as string));
-        }
+        const loadLastCoords = async () => {
+            // Check if lat and lon are passed as params
+            if (params.lat && params.lon) {
+                const lat = parseFloat(params.lat as string);
+                const lon = parseFloat(params.lon as string);
+                setLats(lat);
+                setLongs(lon);
+    
+                await AsyncStorage.setItem('lastLocation', JSON.stringify({ lat, lon }));
+            } else {
+                // If not, load the last location from AsyncStorage
+                const stored = await AsyncStorage.getItem('lastLocation');
+                if (stored) {
+                    const { lat, lon } = JSON.parse(stored);
+                    setLats(lat);
+                    setLongs(lon);
+                }
+            }
+        };
+    
+        loadLastCoords();
     }, [params]);
 
     return (
